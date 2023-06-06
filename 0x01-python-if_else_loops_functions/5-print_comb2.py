cae6fadf7c11e3ab8e0adafb53e3904a31e2c6ee
younesapi <younes.sh1234@gmail.com>
2023-06-06 22:20:21 +0000

    print numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if (num >= 0) and (num <= 9):
-        print("0{0:d}, ".format(num), end='')
-    elif (num > 9) and (num <= 98):
-        print("{0:d}, ".format(num), end='')
-    else:
-        print("{0:d} ".format(num), end='\n')
+        if num != 99:
+                print("{:02d}, ".format(num), end='')
+        else:
+                print("{:d}".format(num))
