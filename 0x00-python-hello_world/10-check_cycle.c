bb82ab65ffdabdffd98f8cbcd4e50aa4bfc8343e
younesapi <younes.sh1234@gmail.com>
2023-06-18 15:55:02 +0000

    fixing the 10th task.

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * chech_cycle - function that checks if a single list has a cycle.
- * list: linked list to check.
+ * check_cycle - function that checks if a single list has a cycle.
+ * @list: linked list to check.
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *p;
-	
+
 	if (!list)
 		return (0);
 
