364f04e099acbfe397b725a6cb8d08f5ecb71064
younesapi <younes.sh1234@gmail.com>
2023-06-18 15:50:07 +0000

    adding the technical interview task.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * chech_cycle - function that checks if a single list has a cycle.
+ * list: linked list to check.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *p;
+	
+	if (!list)
+		return (0);
+
+	p = list;
+	while (p)
+	{
+		if (p->next == list)
+			return (1);
+		p = p->next->next;
+	}
+	return (0);
+}
