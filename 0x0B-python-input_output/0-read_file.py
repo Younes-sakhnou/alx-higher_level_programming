79e2c20fb2683d9689febad4e924d95b15d1aedc
Younes-sakhnou <younes.sh1234@gmail.com>
2023-07-11 19:20:24 +0000

    fixing the pycodestyle for the first task.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Text file-reading function."""
 
+
 def read_file(filename=""):
     """this function reads texts file and prints it to the stdout."""
     with open(filename, encoding="utf-8") as myFile:
