ccb9f9c139ce3468b80cc43eb075bf3ce191a396
Younes-sakhnou <younes.sh1234@gmail.com>
2023-07-11 19:18:56 +0000

    fixing the first task function.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""Text file-reading function."""
 
 def read_file(filename=""):
     """this function reads texts file and prints it to the stdout."""
-
-
-with open("my_file_0.txt", encoding="utf-8") as myFile:
-    print(myFile.read(), end="")
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read(), end="")
