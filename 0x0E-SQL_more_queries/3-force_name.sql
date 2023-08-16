2bfaac4cc449c6e97a9385a94575dfd244b9b37f
Younes-sakhnou <younes.sh1234@gmail.com>
2023-08-16 23:28:39 +0000

    Script that creates the table force_name on your MySQL server.

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Defines the 'force_name' table with an 'id' column (INT) and a 'name' column (VARCHAR, NOT NULL)
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
