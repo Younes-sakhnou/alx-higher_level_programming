59da1643b43ef20ee9809baed704774758cb3ad3
Younes-sakhnou <younes.sh1234@gmail.com>
2023-08-17 00:30:53 +0000

    Script that lists all Comedy shows in the database hbtn_0d_tvshows.

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- Retrieves comedy shows from 'hbtn_0d_tvshows', ordered by show title.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
