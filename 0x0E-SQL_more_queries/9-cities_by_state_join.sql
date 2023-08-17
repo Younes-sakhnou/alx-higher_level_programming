0b5b6dc9b147019104db658a71264bb037ecda00
Younes-sakhnou <younes.sh1234@gmail.com>
2023-08-17 00:09:28 +0000

    Script that lists all cities contained in the database hbtn_0d_usa.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Retrieves all cities and their corresponding states from the 'hbtn_0d_usa' database, sorted by cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
